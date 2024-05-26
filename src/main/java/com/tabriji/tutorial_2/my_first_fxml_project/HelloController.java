5716f1b56b030341b07773045f7837c65901c5e7
Kazi Md. Rakibul Hassan <2220936@iub.edu.bd>
2024-05-26 20:24:21 +0600

    first

---
+++ src/main/java/com/tabriji/tutorial_2/my_first_fxml_project/HelloController.java
@@ -0,0 +1,14 @@
+package com.tabriji.tutorial_2.my_first_fxml_project;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
